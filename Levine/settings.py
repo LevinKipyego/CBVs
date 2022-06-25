39a4e83b34c12f4040b2e5fcec6dccdded4c6f21
levine <levinkipyego@gmail.com>
2022-06-25 09:51:24 +0300

    CBVs

--- Levine/settings.py
+++ Levine/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
    #=======myapps======#
+   'blog',
     'myapp',
 ]
 
--- Levine/urls.py
+++ Levine/urls.py
@@ -4,8 +4,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path(r'zuri-admin/',admin.site.urls),
-    path(r'articles/',include("myapp.urls")),
+    path(r'admin/',admin.site.urls),
+    path("blog/", include("blog.urls", namespace="blog")),
+    path(r'articles/',include("myapp.urls")),   
     path(r'about/',views.about),
     path(r'',views.homepage),
 ]
